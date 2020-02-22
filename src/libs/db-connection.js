3f1febd0ae6945e4600d29959cb5899e7263baef
davidgonzalezfx <dvdramos16@gmail.com>
2020-02-22 17:54:56 -0500

    [Feature] Using mongodb hosting mLab

--- src/libs/db-connection.js
+++ src/libs/db-connection.js
@@ -4,7 +4,7 @@ let db;
 
 module.exports = function Connection() {
 	if(!db){
-		mongoose.connect('mongodb://localhost/crud-example');
+		mongoose.connect('mongodb://davidgonzalezfx:mlab123@ds251948.mlab.com:51948/todoapp');
 		db = mongoose.connection;
 		return db;
 	}
